718af8f2390d2d0b87c2fcdd6c866d589e636a23
Marko Kosunen <marko.kosunen@aalto.fi>
2019-04-24 15:20:42 -0700

    Initial commit

--- src/main/scala/f2_signal_definitions/f2_signal_definitions.scala
+++ src/main/scala/f2_signal_definitions/f2_signal_definitions.scala
@@ -1,32 +1,14 @@
-
-// Dsp-block f2_signal_definitions
-// Description here 
-// Inititally written by dsp-blocks initmodule.sh, 20190424
+//Consider using "unit" instead of "User" 
 package f2_signal_definitions
-
-import chisel3.experimental._
-import chisel3._
-import dsptools._
-import dsptools.numbers._
-import breeze.math.Complex
-
-class f2_signal_definitions_io[T <:Data](proto: T,n: Int) 
-   extends Bundle {
-        val A       = Input(Vec(n,proto))
-        val B       = Output(Vec(n,proto))
-        override def cloneType = (new f2_signal_definitions_io(proto.cloneType,n)).asInstanceOf[this.type]
-   }
-
-class f2_signal_definitions[T <:Data] (proto: T,n: Int) extends Module {
-    val io = IO(new f2_signal_definitions_io( proto=proto, n=n))
-    val register=RegInit(VecInit(Seq.fill(n)(0.U.asTypeOf(proto.cloneType))))
-    register:=io.A
-    io.B:=register
+class usersigs (val n: Int, val users: Int=4) extends Bundle {
+    val udata=DspComplex(SInt(n.W), SInt(n.W))
+    val uindex=UInt(log2Ceil(users).W)
 }
 
-//This gives you verilog
-object f2_signal_definitions extends App {
-    chisel3.Driver.execute(args, () => new f2_signal_definitions(
-        proto=DspComplex(UInt(16.W),UInt(16.W)), n=8) 
-    )
+class iofifosigs(val n: Int, val users: Int=4 ) extends Bundle {
+        //4=Users
+        val data=Vec(users,new usersigs(n=n,users=users))
+        val rxindex=UInt(2.W)
+        override def cloneType = (new iofifosigs(n,users)).asInstanceOf[this.type]
 }
+
